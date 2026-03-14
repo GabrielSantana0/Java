7f6457920daeedcac66387508d0d84666a8b0801
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2026-03-14 17:35:58 -0300

    Avanço nos estudos

---
+++ src/Desafio2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Desafio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = "Jacqueline Oliveira";
+        String conta = "Corrente";
+        int saldoInicial = 2500;
+
+        System.out.println("MENU");
+        System.out.println("1 - Consultar Saldos");
+        System.out.println("2 - Receber valor");
+        System.out.println("3 - Transferir valor");
+        System.out.println("4 - Sair");
+        System.out.println("Escolha uma opção: ");
+        int opcao = scanner.nextInt();
+
+        while(opcao != 4) {
+            if (opcao == 1) {
+                System.out.println(saldoInicial);
+                break;
+            } else if (opcao == 2) {
+                System.out.println("Digite o valor de deposito a receber: ");
+                int deposito = scanner.nextInt();
+                deposito += saldoInicial;
+                System.out.println("Saldo atualizado: $" + deposito);
+                break;
+            } else if (opcao == 3) {
+                System.out.println("Digite o valor de transferir:");
+                int transferir = scanner.nextInt();
+
+                if (transferir > saldoInicial) {
+                    System.out.println("Saldo insuficiente");
+                } else {
+                    saldoInicial -= transferir;
+                    System.out.println("Saldo atualizado: $" + saldoInicial);
+                }
+                break;
+            }
+            else {
+                System.out.println("Opção inválida!");
+                break;
+            }
+        }
+    }
+
+}
