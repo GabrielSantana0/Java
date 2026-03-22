4b1c49570f7b67f3371f5a32057e554a06dbe373
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2026-03-22 18:51:33 -0300

    Avanço nos estudos

--- src/Desafio/notas.java
+++ src/Desafio/notas.java
@@ -1,20 +1,33 @@
 public class notas {
-    public static void main(String[] args) {
-        Aluno aluno1 = new Aluno("Lulu", 7.5, 8.5, 9.2);
-        Aluno aluno2 = new Aluno("Amanda", 3.4, 7.0, 8.5);
+    static void main(String[] args) {
+       Aluno aluno1 = new Aluno("Roberto", 8.5,9.0, 4.5);
+       Aluno aluno2 = new Aluno("Fravin do pneu", 7.5, 2.9, 4);
 
-        System.out.println("Aluno 1: " + aluno1.getNome());
-        System.out.println("Nota 1: " + aluno1.getNota1());
-        System.out.println("Nota 2: " + aluno1.getNota2());
-        System.out.println("Nota 3: " + aluno1.getNota3());
-        System.out.println("Media: " + aluno1.calcularMedia());
+        System.out.println("Aluno1 nome: " + aluno1.getNome());
+        System.out.println("Aluno1 nota1: " + aluno1.getNota1());
+        System.out.println("Aluno1 nota2: " + aluno1.getNota2());
+        System.out.println("Aluno1 nota3: " + aluno1.getNota3());
+        double media1 = aluno1.calcularMedia();
+        System.out.println("Calcular Media: " + media1);
+        if (media1 < 6) {
+            System.out.println("Status: REPROVOU");
+        } else {
+            System.out.println("Status: PASSOU");
+        }
         System.out.println();
 
-        System.out.println("Aluno 2: " + aluno2.getNome());
-        System.out.println("Nota 1: " + aluno2.getNota1());
-        System.out.println("Nota 2: " + aluno2.getNota2());
-        System.out.println("Nota 3: " + aluno2.getNota3());
-        System.out.println("Media: " + aluno2.calcularMedia());
+        System.out.println("Aluno nome: " + aluno2.getNome());
+        System.out.println("Aluno2 nota1: " + aluno2.getNota1());
+        System.out.println("Aluno2 nota2: " + aluno2.getNota2());
+        System.out.println("Aluno2 nota3: " + aluno2.getNota3());
+       double media2 = aluno2.calcularMedia();
+        System.out.println("Calcular Media: " + media2);
 
+        if (media2 < 6 ) {
+            System.out.println("REPROVOU");
+        } else {
+            System.out.println("PASSOU");
+        }
     }
 }
+
