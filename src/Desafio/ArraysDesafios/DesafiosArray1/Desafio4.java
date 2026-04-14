cfca81da06f8b8514d173b7a3e502fdcb500201a
Gabriel santana <157196006+GabrielSantana0@users.noreply.github.com>
2026-04-14 18:01:28 -0300

    Avanço nos estudos

--- src/Desafio/ArraysDesafios/DesafiosArray1/Desafio4.java
+++ src/Desafio/ArraysDesafios/DesafiosArray1/Desafio4.java
@@ -1,4 +1,28 @@
 package Desafio.ArraysDesafios.DesafiosArray1;
 
+import java.util.Scanner;
+
 public class Desafio4 {
+   public  static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int impares = 0;
+       int par = 0;
+
+       System.out.println("Quantidades de Numeros: ");
+       int quantidade = sc.nextInt();
+
+       for (int i = 0; i < quantidade; i++) {
+           System.out.println("Digite Os Numeros: ");
+           int numeros = sc.nextInt();
+
+           if (numeros % 2 == 0) {
+               par ++;
+           }
+           else {
+               impares ++;
+           }
+       }
+       System.out.println("Quantidade par: " + par);
+       System.out.println("Quantidade Impar " + impares);
+    }
 }
--- src/br/com/alura/screenmatch/Principal.java
+++ src/br/com/alura/screenmatch/Principal.java
@@ -10,8 +10,7 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -24,20 +23,18 @@ public class Principal {
         //meuFilme.somaDasAvaliacoes = 10;
         //meuFilme.totalDeAvaliacoes = 1;
         //System.out.println(meuFilme.pegaMedia());
-
-
         System.out.println();
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+
+
+
+        Serie lost = new Serie("Lost", 2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para Maratonar lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme("Predador");
-        outroFilme.setAnoDeLancamento(2015);
+        Filme outroFilme = new Filme("Predador", 2015);
         outroFilme.setDuracaoEmMinutos(150);
 
 
@@ -59,10 +56,9 @@ public class Principal {
         filtro.filtra(episodio);
 
 
-        var filmeDoPaulo = new Filme("Dogville");
+        var filmeDoPaulo = new Filme("Dogville", 2003);
         // filmeDoPaulo.setNome("Dogville");
         filmeDoPaulo.setDuracaoEmMinutos(200);
-        filmeDoPaulo.setAnoDeLancamento(2003);
         filmeDoPaulo.avalia(10);
         System.out.println()
 
